c5947f329e040fdd04bc8860f24eea3bc0103a7c
Kevin Filipak <kevinaguiar09@gmail.com>
2023-04-18 07:58:45 -0300

    Web

--- SistemaMonitoramento.API/Program.cs
+++ SistemaMonitoramento.API/Program.cs
@@ -7,27 +7,20 @@ IConfiguration configuration = new ConfigurationBuilder()
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IContext, Context>();
-
 
 var app = builder.Build();
 
 AppSettingsHelper.AppSettingsConfigure(configuration);
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
