52b9d7d95edc87be51820acd310bd3cc48d8b0c4
Kevin Filipak <kevinaguiar09@gmail.com>
2023-05-31 19:44:03 -0300

    ConnectString

--- SistemaMonitoramento.Sql/Procedures/tb_dashboard/sp_Select_Dashboard.sql
+++ SistemaMonitoramento.Sql/Procedures/tb_dashboard/sp_Select_Dashboard.sql
@@ -9,12 +9,29 @@ CREATE PROCEDURE sp_Select_Dashboard
 AS
 SET NOCOUNT OFF;
 
-    SELECT dispositivo_s_dispositivo	AS dashboard_s_dispositivo
-	     , '25 °C'							AS dashboard_s_temperatura
-         , '30 %'							AS dashboard_s_umidade 
-      FROM tb_dispositivo (NOLOCK)
+    SELECT dispositivo_s_dispositivo				AS dashboard_s_dispositivo
+	     , dispositivo_i_id					
+	     , CONVERT(VARCHAR(100),'')					AS dashboard_s_temperatura
+         , CONVERT(VARCHAR(100),'')					AS dashboard_s_umidade 
+	  INTO #TMP
+      FROM tb_dispositivo A(NOLOCK)
      WHERE dispositivo_b_deleted = 0 
 	   AND dispositivo_s_status = 'Ativo'
 
+	UPDATE #TMP SET dashboard_s_temperatura = (SELECT CONVERT(VARCHAR(20),(SELECT TOP 5 ROUND(AVG(monitoramento_f_temperatura),2) 
+	                                                                         FROM tb_monitoramento 
+																		 	WHERE monitoramento_i_dispositivo = dispositivo_i_id)) + ' °C')
+
+	UPDATE #TMP SET dashboard_s_umidade =     (SELECT CONVERT(VARCHAR(20),(SELECT TOP 5 ROUND(AVG(monitoramento_f_umidade),2)     
+	                                                                         FROM tb_monitoramento 
+																			WHERE monitoramento_i_dispositivo = dispositivo_i_id)) + ' %')
+
+	SELECT *
+	  FROM #TMP
+
+	DROP TABLE #TMP
+
 SET NOCOUNT ON;
 GO
+
+exec sp_Select_Dashboard ''
