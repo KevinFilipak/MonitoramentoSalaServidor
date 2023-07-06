3fa9b95a0ba72350b6bb76f4aa3882207e68b8c4
Kevin Filipak <kevinaguiar09@gmail.com>
2023-07-06 08:53:07 -0300

    Alteração

--- SistemaMonitoramento.Util/Servicos/Email.cs
+++ SistemaMonitoramento.Util/Servicos/Email.cs
@@ -19,10 +19,10 @@ namespace SistemaMonitoramento.Util.Servicos
         public static string Enviar(string emaiL_s_destinatario,string email_s_assunto, string email_s_corpo)
         {
 
-            string email_s_remetente = "contato@garagem41.com";
-            string email_s_senha = "Chevette#41";
+            string email_s_remetente = "#EMAIL#";
+            string email_s_senha = "#SENHA#";
 
-            SmtpClient client = new SmtpClient("smtp.umbler.com", 587);
+            SmtpClient client = new SmtpClient("#SERVIDORDEEMAIL#", 587);
             client.EnableSsl = true;
             client.UseDefaultCredentials = false;
             client.Credentials = new System.Net.NetworkCredential(email_s_remetente, email_s_senha);
